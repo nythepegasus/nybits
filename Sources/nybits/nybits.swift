539a40fc0362c8c29f7da8c221904bb9b5edceff
ny <mac@nythepegas.us>
2024-08-20 04:33:15 -0400

    Current test

--- Sources/nybits/nybits.swift
+++ Sources/nybits/nybits.swift
@@ -1,2 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public extension Data {
+    typealias Byte = UInt8
+    typealias Word = UInt16
+    typealias Dword = UInt32
+    typealias Qword = UInt64
+    
+}
+
+public extension UInt8 {
+    func check(_ bit: Int) -> Bool {
+        return (self & (1 << bit)) != 0
+    }
+}
+
