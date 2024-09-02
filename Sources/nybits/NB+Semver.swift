33c633ff6a6b2a6dd77a9c9058b499fece17a32a
ny <mac@nythepegas.us>
2024-09-02 06:21:34 -0400

    Attempt this idea

---
+++ Sources/nybits/NB+Semver.swift
@@ -0,0 +1,29 @@
+//
+//  NB+Semver.swift
+//  nybits
+//
+//  Created by ny on 8/22/24.
+//
+
+import Foundation
+
+#if canImport(Semver)
+
+import Semver
+
+extension Semver {
+    func nextPatch(_ versions: Int = 1) -> Semver { Semver(major: self.major, minor: self.minor, patch: self.patch + versions) }
+    var nextPatch: Semver {nextPatch() }
+    
+    func nextMinor(_ versions: Int = 1) -> Semver { Semver(major: self.major, minor: self.minor + versions, patch: 0) }
+    var nextMinor: Semver { nextMinor() }
+    
+    func nextMajor(_ versions: Int = 1) -> Semver { Semver(major: self.major + versions, minor: 0, patch: 0) }
+    var nextMajor: Semver { nextMajor() }
+    
+    static var buh: Semver {
+        Semver(major: 0, minor: 0, patch: 1, prerelease: ["0"], buildMetadata: ["Buh"])
+    }
+}
+
+#endif
