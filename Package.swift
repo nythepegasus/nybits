1258aeac51ce8c2a494b89fe0528503c9a821d05
ny <mac@nythepegas.us>
2024-09-24 22:52:26 -0400

    Add example to disable Defaultable conformances

--- Package.swift
+++ Package.swift
@@ -12,7 +12,13 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-docc-plugin", branch: "main"),
     ],
     targets: [
-        .target(name: "nybits"),
+        .target(
+            name: "nybits",
+            swiftSettings: [
+                ///.define("DISABLE_FOUNDATION_DEFAULTABLE")
+                /// Define this on your own target to disable ny's default `Defaultable` implementations
+            ]
+        ),
         .executableTarget(name: "nytester", dependencies: ["nybits"]),
         .testTarget(
             name: "nybitsTests",
