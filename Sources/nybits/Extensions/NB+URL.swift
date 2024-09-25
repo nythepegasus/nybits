de095207f36a12d5f657bedc000ca5df26b3ab8d
ny <mac@nythepegas.us>
2024-09-25 02:39:53 -0400

    Make URL extension public

--- Sources/nybits/Extensions/NB+URL.swift
+++ Sources/nybits/Extensions/NB+URL.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-extension URL {
+public extension URL {
     init(staticString string: StaticString) {
         guard let url = URL(string: "\(string)") else {
             preconditionFailure("Invalid URL StaticString: \(string)")
