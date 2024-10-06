4314cd8e2c91f39bcb5222491a141891a11f3c2f
ny <mac@nythepegas.us>
2024-10-06 04:24:38 -0400

    Add mutating variation of these

--- Sources/nybits/Extensions/NB+Int.swift
+++ Sources/nybits/Extensions/NB+Int.swift
@@ -111,9 +111,17 @@ public extension FixedWidthInteger {
         return o.overflow ? .max : o.partialValue
     }
     
+    mutating func addingWithoutOverflow(_ other: Self) {
+        self = addWithoutOverflow(other)
+    }
+    
     func subtractWithoutOverflow(_ other: Self) -> Self {
         let o = subtractingReportingOverflow(other)
         return o.overflow ? .min : o.partialValue
+    }
+    
+    mutating func subtractingWithoutOverflow(_ other: Self) {
+        self = subtractWithoutOverflow(other)
     }
 }
 
