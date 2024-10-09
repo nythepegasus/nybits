292875bf4ecc660cbd263f93b154850a52ce07b2
ny <mac@nythepegas.us>
2024-10-09 19:22:53 -0400

    Add isDirectory implementation

--- Sources/nybits/Extensions/NB+URL.swift
+++ Sources/nybits/Extensions/NB+URL.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 public extension URL {
+    var isDirectory: Bool {
+        var b: ObjCBool = false
+        return FileManager.default.fileExists(atPath: path, isDirectory: &b) && b.boolValue
+    }
+
+
     init(staticString string: StaticString) {
         guard let url = URL(string: "\(string)") else {
             preconditionFailure("Invalid URL StaticString: \(string)")
