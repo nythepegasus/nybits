5eee2b733a3fae7cf66cef540cc0e9b04c52ee8c
ny <mac@nythepegas.us>
2024-10-08 21:53:01 -0400

    Fix iOS building

--- Sources/nysuibits/Extensions/NB+View.swift
+++ Sources/nysuibits/Extensions/NB+View.swift
@@ -13,6 +13,7 @@ import nydefaults
 
 import SwiftUICore
 
+@available(iOS 13.0, tvOS 13.0, watchOS 6.0, *)
 public extension View {
     func onChange<T>(_ values: [T], perform: () -> Void) -> some View where T: Equatable {
         var previousValues: [T] = values
